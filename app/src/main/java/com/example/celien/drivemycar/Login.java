096395c368eb54f260aca82a52d9d4af1d9faac2
Celien <cesliens@gmail.com>
2015-02-23 11:23:54 +0100

    Add a toolbar with custom settings

--- app/src/main/java/com/example/celien/drivemycar/Login.java
+++ app/src/main/java/com/example/celien/drivemycar/Login.java
@@ -40,7 +40,7 @@ public class Login extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.exit) {
             return true;
         }
 
