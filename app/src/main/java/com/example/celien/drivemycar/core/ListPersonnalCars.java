cfa88d78ea8d5f1c4eb67bced0641495c2f1ae3a
Celien <cesliens@gmail.com>
2015-03-04 16:06:13 +0100

    Add a some menu item to offer the possibility to add a new car even if we already have one

--- app/src/main/java/com/example/celien/drivemycar/core/ListPersonnalCars.java
+++ app/src/main/java/com/example/celien/drivemycar/core/ListPersonnalCars.java
@@ -1,5 +1,6 @@
 package com.example.celien.drivemycar.core;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -79,16 +80,29 @@ public class ListPersonnalCars extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id){
+            case R.id.menuAddCar:
+                launchIntent();
+                break;
+            case R.id.menuSettings:
+                break;
+            case R.id.menuExit:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void launchIntent(){
+        Intent i = new Intent(this, AddCar.class);
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("user", user);
+        i.putExtras(bundle);
+        startActivity(i);
+    }
+
+
 }
