998434d2ce835bbde27653c4207ca32fa357dcb5
Celien <cesliens@gmail.com>
2015-04-13 16:13:16 +0200

    Fine tuning on the template

--- app/src/main/java/com/example/celien/drivemycar/tabs/TabOperations.java
+++ app/src/main/java/com/example/celien/drivemycar/tabs/TabOperations.java
@@ -2,6 +2,7 @@ package com.example.celien.drivemycar.tabs;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -38,10 +39,28 @@ public class TabOperations extends Fragment {
     }
 
     private void init(View v){
-
         // Get the notification data from SharedPref and test is there is indeed something.
         HashMap<String, String> notificationData = Tools.getNotificationData(getActivity().getSharedPreferences("notifInfo", Context.MODE_PRIVATE));
         if(notificationData.get("userSource").equals(""))
             Log.e(getClass().getName(), "There is nothing in SharedPref.notifInfo");
+
+        // Get the elements on the layout
+        tvUserSource = (TextView)v.findViewById(R.id.tvUserSource);
+        tvBrand      = (TextView)v.findViewById(R.id.tvBrand);
+        tvModel      = (TextView)v.findViewById(R.id.tvModel);
+        tvFromDate   = (TextView)v.findViewById(R.id.tvDateFrom);
+        tvFromTime   = (TextView)v.findViewById(R.id.tvTimeFrom);
+        tvToDate     = (TextView)v.findViewById(R.id.tvDateTo);
+        tvToTime     = (TextView)v.findViewById(R.id.tvTimeTo);
+
+        // Init their values
+        tvUserSource.setText(notificationData.get("userSource"));
+        tvBrand.setText(notificationData.get("brand"));
+        tvModel.setText(notificationData.get("model"));
+        tvFromDate.setText(notificationData.get("fromDate").substring(0, 9));
+        tvFromTime.setText(notificationData.get("fromDate").substring(10, notificationData.get("fromDate").length() - 5)+ " h");
+        tvToDate.setText(notificationData.get("toDate").substring(0, 9));
+        tvToTime.setText(notificationData.get("toDate").substring(10, notificationData.get("toDate").length() - 5)+ " h");
+
     }
 }
