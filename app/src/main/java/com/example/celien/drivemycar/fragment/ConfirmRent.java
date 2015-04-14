d12fce15e2c1a7e8038af5a78eaffe6aef6ba72c
Celien <cesliens@gmail.com>
2015-04-14 16:00:18 +0200

    Fine tuning on Fragmend display

--- app/src/main/java/com/example/celien/drivemycar/fragment/ConfirmRent.java
+++ app/src/main/java/com/example/celien/drivemycar/fragment/ConfirmRent.java
@@ -8,19 +8,63 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.DialogFragment;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.example.celien.drivemycar.R;
 
 public class ConfirmRent extends DialogFragment{
 
+    private EditText etMileage;
+    private Button btnConfirm;
+    private Button btnCancel;
+
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState);
+        Dialog dialog =  super.onCreateDialog(savedInstanceState);
+        dialog.setTitle("Confirm rent");
+        return dialog;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_confirm_rent, container, false);
+        init(rootView);
+        setListeners();
         return rootView;
+    }
+
+    private void init(View v){
+        etMileage  = (EditText)v.findViewById(R.id.etSetOdometer);
+        btnConfirm = (Button)v.findViewById(R.id.btnOk);
+        btnCancel  = (Button)v.findViewById(R.id.btnCancel);
+    }
+
+    private void setListeners(){
+        btnConfirm.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                sendOdometerValue();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                dismissDialog();
+            }
+        });
+    }
+
+    private void dismissDialog(){
+        dismiss();
+    }
+
+    private void sendOdometerValue(){
+        Toast.makeText(this.getActivity(), "Confirm has been pressed", Toast.LENGTH_SHORT).show();
     }
 }
