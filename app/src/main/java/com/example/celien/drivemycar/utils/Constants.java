f907790f5f6e419c7df7ed60ff399c54a4302bea
Celien <cesliens@gmail.com>
2015-04-16 14:43:24 +0200

    Reformat some URL style

--- app/src/main/java/com/example/celien/drivemycar/utils/Constants.java
+++ app/src/main/java/com/example/celien/drivemycar/utils/Constants.java
@@ -3,18 +3,19 @@ package com.example.celien.drivemycar.utils;
 
 public class Constants {
 
-    public final static String SAVE_USER_URL        = "http://cafca.ngrok.com/register";
-    public final static String AUTHENTICATE_URL     = "http://cafca.ngrok.com/android/login";
-    public final static String SAVE_CAR_URL         = "http://cafca.ngrok.com/android/save_car";
-    public final static String MODIFY_CAR_URL       = "http://cafca.ngrok.com/android/modify_car";
-    public final static String DELETE_CAR_URL       = "http://cafca.ngrok.com/android/delete_car";
-    public final static String CONFIRM_RENT_URL     = "http://cafca.ngrok.com/android/confirm_rent";
-    public static final String LOAD_USER_URL           = "http://cafca.ngrok.com/android/get_user";
-    public static final String LOAD_CARS_URL           = "http://cafca.ngrok.com/android/get_cars";
-    public static final String CHECK_USER_UNIQUE_URL   = "http://cafca.ngrok.com/android/username_unique";
-    public static final String LOAD_ALL_CARS_BRAND     = "http://cafca.ngrok.com/android/get_all_cars_brand";
-    public static final String LOAD_SPECIFIC_CARS_URL  = "http://cafca.ngrok.com/android/get_specific_cars";
-    public static final String GET_NOTIFS_URL          = "http://cafca.ngrok.com/android/get_notifs";
-    public static final String SAVE_REQUEST_URL = "http://cafca.ngrok.com/android/set_request";
+    /*Every useful URLs to play Server*/
+    public final static String SAVE_USER_URL            = "http://cafca.ngrok.com/register";
+    public final static String AUTHENTICATE_URL         = "http://cafca.ngrok.com/android/login";
+    public final static String SAVE_CAR_URL             = "http://cafca.ngrok.com/android/save_car";
+    public final static String MODIFY_CAR_URL           = "http://cafca.ngrok.com/android/modify_car";
+    public final static String DELETE_CAR_URL           = "http://cafca.ngrok.com/android/delete_car";
+    public final static String CONFIRM_RENT_URL         = "http://cafca.ngrok.com/android/confirm_rent";
+    public static final String LOAD_USER_URL            = "http://cafca.ngrok.com/android/get_user";
+    public static final String LOAD_CARS_URL            = "http://cafca.ngrok.com/android/get_cars";
+    public static final String CHECK_USER_UNIQUE_URL    = "http://cafca.ngrok.com/android/username_unique";
+    public static final String LOAD_ALL_CARS_BRAND      = "http://cafca.ngrok.com/android/get_all_cars_brand";
+    public static final String LOAD_SPECIFIC_CARS_URL   = "http://cafca.ngrok.com/android/get_specific_cars";
+    public static final String GET_NOTIFS_URL           = "http://cafca.ngrok.com/android/get_notifs";
+    public static final String SAVE_REQUEST_URL         = "http://cafca.ngrok.com/android/set_request";
 
 }
