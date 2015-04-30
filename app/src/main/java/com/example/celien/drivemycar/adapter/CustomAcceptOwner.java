927577255cf022aed5c3aaa79d652b3d809978b5
Celien <cesliens@gmail.com>
2015-04-30 16:06:32 +0200

    Bind items on new view

--- app/src/main/java/com/example/celien/drivemycar/adapter/CustomAcceptOwner.java
+++ app/src/main/java/com/example/celien/drivemycar/adapter/CustomAcceptOwner.java
@@ -1,6 +1,76 @@
 package com.example.celien.drivemycar.adapter;
 
 
-public class CustomAcceptOwner {
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
+import com.example.celien.drivemycar.R;
+import com.example.celien.drivemycar.core.AcceptOwner;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class CustomAcceptOwner extends ArrayAdapter<JSONObject> {
+
+    private AcceptOwner caller;
+    private List<JSONObject> list;
+    private JSONObject currentJson;
+    private TextView tvOwnerName;
+    private TextView tvBrand;
+    private TextView tvModel;
+    private CheckBox cbSelectedOwner;
+
+    public CustomAcceptOwner(Context ctxt, List<JSONObject> list, AcceptOwner caller){
+        super(ctxt, R.layout.custom_accept_owner, list);
+        this.caller = caller;
+        this.list   = list;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View v = inflater.inflate(R.layout.custom_accept_owner, parent, false);
+
+        // Get the item from layout
+        currentJson     = getItem(position);
+        tvOwnerName     = (TextView)v.findViewById(R.id.tvOwnerName);
+        tvBrand         = (TextView)v.findViewById(R.id.tvBrand);
+        tvModel         = (TextView)v.findViewById(R.id.tvModel);
+        cbSelectedOwner = (CheckBox)v.findViewById(R.id.cbSelectedOwner);
+
+        // Update value of fields
+        try{
+            tvOwnerName.setText(currentJson.getString("owner"));
+            tvBrand.setText(currentJson.getString("brand"));
+            tvModel.setText(currentJson.getString("model"));
+        }catch(JSONException e){
+            Log.e(e.getClass().getName(), "JSONException", e);
+        }
+
+        return v;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public JSONObject getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 }
+
