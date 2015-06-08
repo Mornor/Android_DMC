d63252e35d00036cc6aaec5e2a8cfe9c74ea2f2b
Celien <cesliens@gmail.com>
2015-06-08 11:10:24 +0200

    Modify Constants to fit to test environment

--- app/src/main/java/com/example/celien/drivemycar/utils/Constants.java
+++ app/src/main/java/com/example/celien/drivemycar/utils/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     private final static String ngrokTunnel = ".ngrok.io";
     private final static String localTunnel = ".localtunnel.me";
     private final static String heroku      = ".herokuapp.com";
-    private final static String tunnelToUse = heroku;
+    private final static String tunnelToUse = ngrokTunnel;
 
     /*Url to use to access to Play! server*/
     public final static String SAVE_USER_URL                = "https://cafca"+tunnelToUse+"/register";
