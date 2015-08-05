be6bbaeee7be81c57b2ead0d276f91e2c3fa6bd8
Celien <cesliens@gmail.com>
2015-08-05 17:18:40 +0200

    Delete few lines

--- app/src/main/java/be/iba/carswop/tabs/TabSearchCar.java
+++ app/src/main/java/be/iba/carswop/tabs/TabSearchCar.java
@@ -21,7 +21,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
